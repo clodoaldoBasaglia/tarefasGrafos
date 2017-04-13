f433e56d4a7ed50c459183b9c5ef7596f4c83939
luisfernandolfc <luisfernnando@gmail.com>
2017-04-12 22:55:05 -0300

    Atualizando BFS

--- src/grafos/BFS/BFS.java
+++ src/grafos/BFS/BFS.java
@@ -14,29 +14,36 @@ import java.util.*;
  */
 public class BFS {
     //
-	Queue q = new LinkedList();
-	
+    public void initBfs(Grafo g)
+    {
+        
+    }
     public void bfs(Grafo g, VerticeBL s)
     {
+        /*Inicializando um map com o grafo e em seguida pegando a lista de adjacência
+         do vértice inical da busca (s)*/
         Map<VerticeBL, ArrayList<VerticeBL>> grafo = g.getGrafo();
-        VerticeBL node = new VerticeBL();
-        node = s;
-        node.setCor("Branco");
+        ArrayList<VerticeBL> vertices = grafo.get(s);
+       
+        vertices.get(0).setCor("Cinza");     
         
-        ArrayList<VerticeBL> get = grafo.get(s);
         
         Queue<VerticeBL> queue = new LinkedList<VerticeBL>();
 
-        if(grafo == null) return;
+        if(vertices.get(0) == null) return;
         
         queue.add(s);
 
+/*        Enquanto a fila não for vazia procurar pelos vértices adjascentes ao inicial
+          que forem da cor branca, colocar a cor como cinza e adicionar na fila para
+            buscar os vertices adjascentes a eles*/
         while(!queue.isEmpty())
         {
            
             VerticeBL r = queue.remove(); 
-
-            for(VerticeBL n: r.getChild())
+            ArrayList<VerticeBL> v = grafo.get(r);
+            
+            for(VerticeBL n: v)
             {
                 if(n.getCor().equals("Branco"))
                 {
